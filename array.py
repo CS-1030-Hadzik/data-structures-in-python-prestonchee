864300096f808f486154871c73fbd6521ccd90d6
prestonchee <prestonchee1@gmail.com>
2022-12-01 18:21:20 -0700

    finished code

--- array.py
+++ array.py
@@ -33,5 +33,6 @@ print(cars)
 # `problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)
 my_array_length = len(cars)
 # `problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
-array_string = 'The length of my array is '
+array_string = 'The length of my array is'
 # `problem 18. print array_string concatenated with my_array_length to the console.`
+print(array_string, my_array_length)
