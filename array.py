4c6e89baef351c07914ce3158973c80159a84531
prestonchee <prestonchee1@gmail.com>
2022-12-01 18:35:56 -0700

    added for loop

--- array.py
+++ array.py
@@ -35,6 +35,17 @@ print(cars)
 my_array_length = len(cars)
 # `problem 17. create a variable called array_string with a value 
 # of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
-array_string = 'The length of my array is'
+array_string = 'The length of my array is '
 # `problem 18. print array_string concatenated with my_array_length to the console.`
-print(array_string, my_array_length)
+print(array_string + str(my_array_length))
+
+#for each
+#cars --> Plural -- whole list
+#car --> singular -- one element
+
+cars.append('Kia')
+print('===========================')
+element = 1
+for car in cars: #len of list
+    print(element, car)
+    element += 1
